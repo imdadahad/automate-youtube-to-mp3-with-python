7b36c1af47de75dcfe008956a8776fdd4e58792a
odunayo12 <odun.rotimi@gmail.com>
2021-01-01 15:08:13 +0100

    Make runnable on Windows OS

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
+# %%
 import subprocess
 import youtube_dl
+import os
+# Unomment if working on windows os
+# subprocess.call(['dir'], shell=True)
 
 
 def run():
@@ -9,6 +13,32 @@ def run():
     video_info = youtube_dl.YoutubeDL().extract_info(
         url=video_url, download=False
     )
+
+    vedio_title = video_info['title']
+    filename = f"{vedio_title}.mp3"
+
+    # Ask the user for the path they want to save the file after it has been downloaded
+    path_to_save = input(
+        r"Enter where the file is to be saved or leave empty if you want it to be saved in the current directory: ")
+    if path_to_save == "":
+        where_to_save = os.getcwd()
+    else:
+        where_to_save = f"{path_to_save}"
+
+    options = {
+        # reduces the several line of output to bearest mininimal and therfore, time
+        'quiet': True,
+        'format': 'bestaudio/best',
+        'keepvideo': False,
+        # f"{where_to_save}\\{filename}"
+        'outtmpl': os.path.join(where_to_save, filename),
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }]
+    }
+
     filename = f"{video_info['title']}.mp3"
     options = {
         'format': 'bestaudio/best',
@@ -26,6 +56,15 @@ def run():
     # Open the file once it has been downloaded
     subprocess.call(["open", filename])
 
+    # Open the file once it has been downloaded (on Mac os)
+    try:
+        subprocess.call(["open", filename])
+    except FileNotFoundError:
+        # this will take care of the windows part
+        subprocess.call(('cmd', '/C', 'start', '', filename))
+
 
 if __name__ == '__main__':
     run()
+
+# %%
