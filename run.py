ede6fb76f4021acb71427f30ce24f0579c103720
odunayo12 <odun.rotimi@gmail.com>
2021-01-03 09:10:08 +0100

    delete code chunck

--- run.py
+++ run.py
@@ -45,19 +45,6 @@ def run():
     }
 
     filename = f"{video_info['title']}.mp3"
-    options = {
-        'format':
-        'bestaudio/best',
-        'keepvideo':
-        False,
-        'outtmpl':
-        filename,
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }]
-    }
 
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([video_info['webpage_url']])
@@ -72,3 +59,5 @@ def run():
 
 if __name__ == '__main__':
     run()
+
+# %%
