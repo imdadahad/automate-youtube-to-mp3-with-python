423e3689614b80f440ccd914aa0644565cab680e
odunayo12 <odun.rotimi@gmail.com>
2021-02-25 12:54:09 +0100

    add mp4 download option

--- Readme.md
+++ Readme.md
@@ -1,17 +1,21 @@
-## How to use
+# How to use
+
 1. Ensure that [`ffmpeg`](https://ffmpeg.org/download.html#releases) is installed on your machine. See guide [here](https://video.stackexchange.com/questions/20495/how-do-i-set-up-and-use-ffmpeg-in-windows).
 2. Install the required library using `$ pip install -r requirements.txt`
-3. Run app with ` $ python run.py`.
+3. Run app with `$ python run.py`.
 4. Enter video `url`.
 5. Enter directory you wish to save your file after it finishes downloading or leave empty to save file in your default working directory.
+6. Enter 1 for audio only, 2 for video only, or 3 for both
+7. Grin *😀*.
 
-# ⚠ Disclaimer
+## Milestones
+
+- [x] Download video.
+- [x] Download both video and audio files.
+- [ ] Download audio and video playlists at a go.
+- [ ] Package as a desktop app.
+- [ ] Incorporate GUI.
+
+## **⚠ Disclaimer**
+
 This app is an illustrative support for a `python` *tasks automation* YouTube [tutorial](https://www.youtube.com/watch?v=FsNPdhWmil4&feature=youtu.be&ab_channel=ImdadAhad) on my [channel](https://www.youtube.com/channel/UC2vT_yPHGqD4rYn104PjBUw). And, therefore, intended for educational purposes only.
-
-Milestone
-- [ ] Take input as mp3 or mp4.
-  * See how to use dictionary [here](https://stackoverflow.com/a/45149883/13119339)
-- [ ] Format `url` to take ranges or points.
-- [ ] Playlist flow - [ ] Yes or - [ ] No. 
-- [ ] Use thinker to build a UI
-  * use checkbox for mp3, mp4 or both
--- run.py
+++ run.py
@@ -29,27 +29,57 @@ def run():
     filename = f"{video_info['title']}.mp3"
     output_path = os.path.join(where_to_save, filename)
 
-    options = {
-        # reduces the several line of output to barest minimal and therefore, time
-        'quiet': True,
-        'noplaylist': True,
-        'format': 'bestaudio/best',
-        'keepvideo': False,
-        'outtmpl': output_path,
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }]
-    }
+    options_bin = input(
+        "Enter 1 for audio only, 2 for video only or 3 for both: ")
 
-    with youtube_dl.YoutubeDL(options) as ydl:
-        ydl.download([video_info['webpage_url']])
+    class Download_Options:
+        def __init__(self, options_bin):
+            #self.media_type = media_type
+            self.options_bin = options_bin
+            #self.target_media = target_media
+            self.video_target = {
+                "noplaylist": True,
+                "quiet": True,
+                "format": "bestvideo",
+            }
+            self.audio_target = {
+                # reduces the several line of output to barest minimal and therefore, time
+                'quiet': True,
+                'noplaylist': True,
+                'format': 'bestaudio/best',
+                'keepvideo': True,
+                'outtmpl': output_path,
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }]
+            }
 
-    # returns os system eg. 'nt' for windows
+        def download_media(self, video_info):
+            """
+            docstring
+            """
+            if self.options_bin == "1":
+                with youtube_dl.YoutubeDL(self.audio_target) as ydl:
+                    ydl.download([video_info['webpage_url']])
+
+            elif self.options_bin == '2':
+                with youtube_dl.YoutubeDL(self.video_target) as ydl:
+                    ydl.download([video_info['webpage_url']])
+
+            elif self.options_bin == '3':
+                for each_item in [self.audio_target, self.video_target]:
+                    with youtube_dl.YoutubeDL(each_item) as ydl:
+                        ydl.download([video_info['webpage_url']])
+
+    audio_media_type = Download_Options(options_bin)
+    audio_media_type.download_media(video_info)
+
+    # # returns os system eg. 'nt' for windows
     coding_env = os.name
 
-    # Open the file once it has been downloaded
+    # # Open the file once it has been downloaded
     os.startfile(filename) if coding_env == 'nt' else subprocess.call(
         ["open", filename])
 
